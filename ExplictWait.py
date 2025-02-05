c04aa48f5eade8bf336a7bd3c485479020b05048
Oak <oaksoe146@gmail.com>
2025-02-05 13:49:48 +0700

    Wait Exercise

---
+++ ExplictWait.py
@@ -0,0 +1,46 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+actuallist = []
+
+driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("ber")
+
+time.sleep(5)
+results = driver.find_elements(By.XPATH, "//div[@class='products']/div")
+count = len(results)
+assert count > 0
+for result in results:
+    result.find_element(By.XPATH, "div/button").click()
+    actuallist.append(result.find_element(By.XPATH, "h4").text)
+
+print(actuallist)
+
+driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
+driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
+
+#validation
+prices = driver.find_elements(By.CSS_SELECTOR,"tr td:nth-child(5) p")
+sum = 0
+for price in prices:
+    sum = sum + int(price.text)
+
+print(sum)
+totalAmount = int(driver.find_element(By.CSS_SELECTOR, ".totAmt").text)
+assert totalAmount == sum
+
+
+driver.find_element(By.CLASS_NAME, "promoCode").send_keys("rahulshettyacademy")
+driver.find_element(By.CLASS_NAME, "promoBtn").click()
+wait = WebDriverWait(driver,10)
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".promoInfo")))
+print(driver.find_element(By.CSS_SELECTOR, ".promoInfo").text)
+
+AfterDis = float(driver.find_element(By.CSS_SELECTOR,".discountAmt").text)
+assert totalAmount > AfterDis
